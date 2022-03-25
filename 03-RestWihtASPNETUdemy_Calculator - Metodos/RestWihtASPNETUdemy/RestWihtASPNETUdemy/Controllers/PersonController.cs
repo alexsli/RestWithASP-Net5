cbcd7d3245bd3d4274095a9f061b281be50820d5
alexsli <lima.alex.so@GMAIL.COM>
2022-03-24 22:52:43 -0300

    exercicio Person

---
+++ 03-RestWihtASPNETUdemy_Calculator - Metodos/RestWihtASPNETUdemy/RestWihtASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWihtASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Imput");
+        }
+
+
+        private bool IsNumeric(string strNumber)
+        {
+            double Number;
+            bool isNumber = double.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out Number);
+            return isNumber;
+            //throw new NotImplementedException();
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            return 0;
+        }
+
+    }
+}
